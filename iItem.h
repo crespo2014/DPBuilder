5235e24e0db06d353612708f7474917c6b95c7be
Lester <crespo2014@users.noreply.github.com>
2015-03-26 15:37:38 +0000

    Added more functions to Item interface

--- iItem.h
+++ iItem.h
@@ -8,6 +8,12 @@
 #ifndef IITEM_H_
 #define IITEM_H_
 
+/**
+ * Substring does not support point to invalid location
+ * size == 0 means zero end string.
+ * ptr == null means empty string
+ * characters are const
+ */
 enum class item_type 
 {
   none,
@@ -28,7 +34,23 @@ enum item_flags
 };
 class Iitem
 {
-  const char* getName();
+public:
+  const SubString& getName() const = 0;
+  Iitem& createChild(const SubString& name) = 0;
+  /** 
+   * No more data for this item, validate it
+   * false if item does not validate fine
+   */
+  bool close(const SubString& name) = 0;
+  /**
+   * Return false if value was already set
+  */
+  bool Set(const std::string& value) = 0; // Item has to interpret this
+  /**
+   * Item has to interpret this
+   */
+  bool Set(const SubString& value) = 0;
+  //Set(T& val) - it is hard to figure out with method call base on type
 protected:
   /// not allowed to delete object throught the interface
   virtual ~Iitem() {};
