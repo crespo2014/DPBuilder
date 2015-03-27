f396ae80cd5f4d702b236710bbe6c7ab57f3fd03
Lester <crespo2014@users.noreply.github.com>
2015-03-27 10:57:48 +0000

    Update basicItem.h

--- basicItem.h
+++ basicItem.h
@@ -11,13 +11,45 @@
 #include "iItem.h"
 
 /**
- * Basic iItem implementation that implements basic behaviour
+ * Class to convert from string to numeric types
  */
+template<class T>
+class fromString
+{
+  
+};
 
+/**
+ * Basic iItem implementation that implements basic behaviour
+ * All functions will throw unimplements exception
+ */
 class basicItem : public Iitem
 {
+  public:
+   basicItem(const char* name) : name_(name){}
+  private:
+    const char* name_;  ///< item name
 
 };
+/**
+ * this class implements set mehods
+ */ 
+template<class T>
+class singleItem 
+{
+  
+};
+
+template<class T>
+class containerItem
+{
+  
+};
+
+class ObjectItem
+{
+};
+
 
 
 
